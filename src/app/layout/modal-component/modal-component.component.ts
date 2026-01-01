caebc922fb496a1b650f498680b499482bdea835
User <sa2467061@gmail.com>
2026-01-01 11:14:13 +0800

    all component

---
+++ src/app/layout/modal-component/modal-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal-component.component.html',
+  styleUrl: './modal-component.component.css'
+})
+export class ModalComponentComponent {
+
+}
