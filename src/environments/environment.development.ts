20c2c8edf5bbfa5351e1dd0126e3a850201edec4
User <sa2467061@gmail.com>
2026-01-01 16:55:44 +0800

    latest

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  production: false,
-  supabaseUrl: 'https://pvzkavqxgjdsiimpftvr.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2emthdnF4Z2pkc2lpbXBmdHZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM1NzEyNDUsImV4cCI6MjAyOTE0NzI0NX0.Bs4NibZa2YXzFlshPqbCszH4yoBzsie0YN4VYU4E-xA',
+    production: false,
+    supabaseUrl: 'https://jlioqqkcxvjwoumkxxnm.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsaW9xcWtjeHZqd291bWt4eG5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMTgxMzYsImV4cCI6MjA4MjU5NDEzNn0._KuLd8MxEpBIe3LJ-akH3jaxlLtXV7hx619F76DHoS8',
 };
